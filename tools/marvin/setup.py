d80821a4eb763b631bccca7d0306e2a4d2cdfeb8
Fabricio Archanjo <farchanjo@gmail.com>
2026-04-24 18:38:08 +0000

    chore(release): bump version to 4.24.1.7-SNAPSHOT

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.24.1.5"
+VERSION = "4.24.1.6"
 
 setup(name="Marvin",
       version=VERSION,
