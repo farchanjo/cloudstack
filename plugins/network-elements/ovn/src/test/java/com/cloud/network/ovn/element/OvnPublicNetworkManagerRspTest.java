13b92f5059663de3a91bbc00bb6f6ee1353ab90e
Fabricio Archanjo <farchanjo@gmail.com>
2026-07-04 20:54:20 -0300

    style(ovn): drop unused anyLong import in OvnPublicNetworkManagerRspTest
    Unused static import flagged by checkstyle (UnusedImportsCheck), blocking the module build.

--- plugins/network-elements/ovn/src/test/java/com/cloud/network/ovn/element/OvnPublicNetworkManagerRspTest.java
+++ plugins/network-elements/ovn/src/test/java/com/cloud/network/ovn/element/OvnPublicNetworkManagerRspTest.java
@@ -17,7 +17,6 @@
 package com.cloud.network.ovn.element;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
